ae5e782ac50e03ac1f999ec4153bca3795f6c2dd
YOUR NAME <YOUR@EMAIL.com>
2021-06-15 14:56:59 -0400

    seeding

--- db/migrate/20210615151055_create_resources.rb
+++ db/migrate/20210615151055_create_resources.rb
@@ -1,7 +1,7 @@
 class CreateResources < ActiveRecord::Migration[6.1]
   def change
     create_table :resources do |t|
-      t.string :type
+      t.string :postType
       t.integer :postScore
       t.string :content
       t.string :source
